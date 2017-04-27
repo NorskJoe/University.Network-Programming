e8e680d66dd66e4e8bd94d4b535cb2e3f1294a10
Joe Johnson <s3542413@student.rmit.edu.au>
2017-04-27 20:02:07 +1000

    Started part one of assignment.

--- src/assignment/Client.java
+++ src/assignment/Client.java
@@ -1,10 +1,78 @@
 package assignment;
 
-public class Client {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/**
+ * 
+ * Client class is the 'player' of the game.  One player per client connection
+ * 
+ * @author Joseph Johnson
+ *
+ */
+public class Client 
+{
+	// port number used to connect to server
+	final static int PORT_NUMBER = 12413;
 
+	public static void main(String[] args) 
+	{
+		// Setting up socket - connecting to server
+		try 
+		{
+			// Connecting to the server via a socket
+			String localAddress = InetAddress.getLocalHost().getHostAddress().toString();
+			Socket socket = new Socket(localAddress, PORT_NUMBER);
+			
+			// get random number to start the game
+			int randomInt = generateNumber();
+			
+			// TODO: put this in a function?
+			// Setup outputstream for sending message to the server
+			OutputStream os = socket.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			BufferedWriter bw = new BufferedWriter(osw);
+			
+			// send random number to the server
+			toServer(bw, randomInt);
+			
+			// Get guess from the player
+			
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param bw: a buffered writer output stream to send messages to server
+	 * @param newInt: the number being sent to server
+	 * @throws IOException
+	 */
+	private static void toServer(BufferedWriter bw, int newInt) throws IOException 
+	{
+		String message = newInt + "\n";
+		bw.write(message);
+		bw.flush();
+	}
+
+	/**
+	 * 
+	 * @return a random int between 0 and 2
+	 */
+	private static int generateNumber() 
+	{
+		Random generator = new Random();
+		return generator.nextInt(3);
 	}
 
 }
--- src/assignment/Server.java
+++ src/assignment/Server.java
@@ -1,10 +1,89 @@
 package assignment;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+import java.util.logging.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/**
+ * 
+ * Server class acts as the game administrator.  Runs all the logic
+ * of the game and communicates results to client
+ * 
+ * @author Joseph Johnson
+ *
+ */
+public class Server 
+{
+	// Loggers used to log all information to relevant log files.  One for communication, one for game information
+	private static final Logger commLogger = Logger.getLogger(Server.class.getName() + "communication");
+	private static final Logger gameLogger = Logger.getLogger(Server.class.getName() + "game");
+	// port number used to connect to client
+	final static int PORT_NUMBER = 12413;
+	// boolean to stop server from running
+	static boolean shutdown = false;
+		
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			// Setup loggers to write to appropriate files
+			setupLoggers();
 
+			// Create socket for server, and listen for any client sockets
+			ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+			
+			// Generate a random number
+			int randomInt = generateNumber();
+			gameLogger.info("Server generated number: " + randomInt);
+			
+			while(!shutdown)
+			{
+				// Connect to client
+				Socket clientSocket = serverSocket.accept();
+				commLogger.info("Connected to client: " + clientSocket.getRemoteSocketAddress().toString());
+				
+				// Receive message from the client
+				InputStream is = clientSocket.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				
+				int received = Integer.parseInt(br.readLine());
+				
+				gameLogger.info("Client generated number: " + received);
+				
+			}
+			
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private static int generateNumber() 
+	{
+		Random generator = new Random();
+		return generator.nextInt(3);
+	}
+
+	private static void setupLoggers() throws SecurityException, IOException 
+	{
+		FileHandler comFile = new FileHandler("communication.log");
+		FileHandler gameFile = new FileHandler("game.log");
+		comFile.setFormatter(new SimpleFormatter());
+		gameFile.setFormatter(new SimpleFormatter());
+		commLogger.addHandler(comFile);
+		gameLogger.addHandler(gameFile);
+		commLogger.setUseParentHandlers(false);
+		gameLogger.setUseParentHandlers(false);
+		
 	}
 
 }
