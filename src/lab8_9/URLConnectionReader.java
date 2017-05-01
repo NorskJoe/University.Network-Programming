5bfba74ce784a16ea1d0c5b2adfde471d5b1a21a
Joe <s3542413@student.rmit.edu.au>
2017-05-01 17:49:39 +1000

    Finished Q1 of lab8_9.  Works on servers, might need to seperate a and b

--- src/lab8_9/URLConnectionReader.java
+++ src/lab8_9/URLConnectionReader.java
@@ -8,6 +8,9 @@ import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class URLConnectionReader {
 
@@ -17,6 +20,8 @@ public class URLConnectionReader {
 		HttpURLConnection connection = null;
 		URL url = null;
 		
+		// Create new URL and connect to it
+		// "http://m1-c45n1.csit.rmit.edu.au/~Course/index.php" THIS IS STORED FOR EASIER TESTING/COPY-PASTING
 		try
 		{
 			url = new URL("http://www.rmit.edu.au/");
@@ -29,38 +34,54 @@ public class URLConnectionReader {
 			e.printStackTrace();
 		}
 
+		// Get the input stream for the connection (only works if getContent() returns an input stream
 		try 
 		{
+			// Similar to using connection.getContent()
 			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		String authority = url.getAuthority();
-		int port = url.getPort();
+		// Get the target IP address by using the hostname
 		String host = url.getHost();
-		String path = url.getPath();
-		System.out.printf("authority: %s \n port: %d \n host: %s \n path: %s \n", authority, port, host, path);
 		System.out.printf("ip address: %s\n", InetAddress.getByName(host).getHostAddress());
 		
-		
-		String contentType = connection.getContentType();
-		int contentLength = connection.getContentLength();
-		System.out.printf("content type is: %s, length is %d\n", contentType, contentLength);
-		
-		for(int n = 0; n < 8; n++)
-		{				
-			System.out.println(connection.getHeaderField(n));
-		}
 
-		StringBuilder builder = new StringBuilder();
-		String line = null;
-		while ((line = in.readLine()) != null)
-		{
-			builder.append(line + "\n");
-		}
 		
+		// Get all the header fields and store them in a map.  Print out the first
+		// 8 header fields and their values
+		Map<String, List<String>> headers = connection.getHeaderFields();
+        Set<Map.Entry<String, List<String>>> entrySet = headers.entrySet();
+        int count = 0;
+        
+        for (Map.Entry<String, List<String>> entry : entrySet) 
+        {
+            String headerName = entry.getKey();
+            System.out.println("HEADER NAME: " + headerName);
+            List<String> headerValues = entry.getValue();
+            
+            for (String value : headerValues) 
+            {
+                System.out.println("HEADER VALUE: " + value);
+            }
+            count++;
+            if(count == 8)
+            {
+            	break;
+            }
+            System.out.println();
+        }
+        
+        // For part b.  Print out all of the data in the URL
+//		StringBuilder builder = new StringBuilder();
+//		String line = null;
+//		while ((line = in.readLine()) != null)
+//		{
+//			builder.append(line + "\n");
+//		}
+//		
 //		System.out.print(builder.toString());
 	}
 
