3195632b7f3418f78d02c5646692ba1e624dfadb
Joe Johnson <s3542413@student.rmit.edu.au>
2017-05-01 16:10:55 +1000

    Q1 working on test websites.  Untested on server

--- src/lab8_9/URLConnectionReader.java
+++ src/lab8_9/URLConnectionReader.java
@@ -1,24 +1,26 @@
-package lab8_9;
+//package lab8_9;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class URLConnectionReader {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		BufferedReader in = null;
-		URLConnection connection = null;
+		HttpURLConnection connection = null;
 		URL url = null;
 		
 		try
 		{
-			url = new URL("http://www.reddit.com/footballhighlights");
-			connection = url.openConnection();
+			url = new URL("http://www.rmit.edu.au/");
+			connection = (HttpURLConnection) url.openConnection();
 			connection.connect();
 
 		} catch (MalformedURLException e) {
@@ -39,9 +41,27 @@ public class URLConnectionReader {
 		int port = url.getPort();
 		String host = url.getHost();
 		String path = url.getPath();
-		
-		
 		System.out.printf("authority: %s \n port: %d \n host: %s \n path: %s \n", authority, port, host, path);
+		System.out.printf("ip address: %s\n", InetAddress.getByName(host).getHostAddress());
+		
+		
+		String contentType = connection.getContentType();
+		int contentLength = connection.getContentLength();
+		System.out.printf("content type is: %s, length is %d\n", contentType, contentLength);
+		
+		for(int n = 0; n < 8; n++)
+		{				
+			System.out.println(connection.getHeaderField(n));
+		}
+
+		StringBuilder builder = new StringBuilder();
+		String line = null;
+		while ((line = in.readLine()) != null)
+		{
+			builder.append(line + "\n");
+		}
+		
+//		System.out.print(builder.toString());
 	}
 
 }
