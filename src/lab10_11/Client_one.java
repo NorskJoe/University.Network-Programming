affe48821250a1591c1e9d619b0911fb8d689c3e
Joe Johnson <s3542413@student.rmit.edu.au>
2017-05-14 15:25:56 +1000

    Client will send a message, server will receive
    No loop, server does not send back message yet.

--- src/lab10_11/Client_one.java
+++ src/lab10_11/Client_one.java
@@ -1,10 +1,85 @@
 package lab10_11;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
 public class Client_one {
+	
+	final int PORT_NUMBER = 12413;
+	private String localAddress;
+	SocketChannel socketChannel;
+	ByteBuffer buffer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		new Client_one();
 
+	}
+	
+	public Client_one()
+	{
+		// Print client information
+		try {
+			printAddressPort();
+			
+		} catch (UnknownHostException e) {
+			System.out.println("Problem when printing client information" + e);
+		}
+		
+		// Open client socket, and connect the server
+		try {
+			openSocket();
+			
+		} catch (IOException e) {
+			System.out.println("There was an error while opening the client socket." + e);
+		}
+		
+		
+		// Get user input and send it to the server
+		try {
+			sendMessage();
+			
+		} catch (IOException e) {
+			System.out.println("There was an error sending a message to the server");
+		}
+		
+		
+	}
+
+	private void sendMessage() throws IOException 
+	{
+		Scanner scanner = new Scanner(System.in);
+		String message = scanner.nextLine();
+		
+		buffer = ByteBuffer.wrap(message.getBytes());
+		
+		socketChannel.write(buffer);
+	}
+
+	private void openSocket() throws IOException 
+	{
+		socketChannel = SocketChannel.open();
+		socketChannel.connect(new InetSocketAddress(PORT_NUMBER));
+		socketChannel.configureBlocking(false);
+		
+		// Since socket is non-blocking, wait until it has connected
+		while(!socketChannel.finishConnect())
+		{
+			System.out.println("Connecting to server...");
+		}
+		
+	}
+
+	private void printAddressPort() throws UnknownHostException 
+	{
+		localAddress = InetAddress.getLocalHost().getHostAddress().toString();
+		System.out.printf("Client is using local address %s.  Port Number %d. \n", localAddress,PORT_NUMBER);
+		
 	}
 
 }
--- src/lab10_11/Server_one.java
+++ src/lab10_11/Server_one.java
@@ -1,10 +1,97 @@
 package lab10_11;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.WritableByteChannel;
+
 public class Server_one {
+	
+	final int PORT_NUMBER = 12413;
+	ServerSocketChannel serverSocketChannel;
+	SocketChannel clientSocketChannel;
+	String localAddress;
+	ByteBuffer buffer;
+	Selector selector;
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		new Server_one();
+	}
+	
+	public Server_one()
+	{
+		// Start the server
+		try {
+			SetupSocket();
+			
+		} catch (IOException e) {
+			System.out.println("There was an error starting the server." + e);
+		}
+		
+		
+		
+		// Print server information
+		try {
+			System.out.printf("The server socket address is %s.\n", serverSocketChannel.getLocalAddress().toString());
+			
+		} catch (IOException e) {
+			System.out.println("There was an error printing the server's information." + e);
+		}
+		
+		
+		
+		// Start the loop of accepting data
+		try {
+			listenForConnections();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	private void listenForConnections() throws IOException 
+	{
+		boolean serverIsRunning = true;
+		while(serverIsRunning)
+		{
+			clientSocketChannel = serverSocketChannel.accept(); // This will block until a connection is made
+			
+			System.out.println("Server received a connection from " + clientSocketChannel.getRemoteAddress());
+			
+			printBuffer();
+		}
+		
+	}
+
+	private void printBuffer() throws IOException 
+	{
+		buffer = ByteBuffer.allocate(256);
+		clientSocketChannel.read(buffer);
+		
+		String message = new String(buffer.array()).trim();
+		
+		System.out.println("Client sent: " + message);
+		
+	}
+
+	private void SetupSocket() throws IOException 
+	{
+		serverSocketChannel = ServerSocketChannel.open();
+		localAddress = InetAddress.getLocalHost().getHostAddress().toString();
+		serverSocketChannel.socket().bind(new InetSocketAddress(localAddress, PORT_NUMBER));
+		serverSocketChannel.configureBlocking(true);
+//		selector.open();
+//		socketChannel.register(selector, SelectionKey.OP_ACCEPT);
+		
 	}
 
 }
